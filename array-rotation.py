55f375ddf22e817ae470fa475d3e1bcad065d134
iarmankhan <armankhan9244@gmail.com>
2020-06-02 14:20:30 +0530

    Array rotation program in python - basic

---
+++ array-rotation.py
@@ -0,0 +1,21 @@
+# Write a program that rotate an array
+
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+n = len(a)
+
+temp = []
+
+rotation = 3
+
+for i in range(0, rotation):
+    temp.append(a[i])
+
+j = 0
+for i in range(0, n):
+    if i + rotation < n:
+        a[i] = a[i + rotation]
+    else:
+        a[i] = temp[j]
+        j += 1
+
+print(a)
